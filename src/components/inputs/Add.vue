fdd45b74b09bd03ed3acc99411aa3df5b6207555
Andres Foronda <andresforonda.af@gmail.com>
2021-01-12 08:04:03 -0300

    Move add input to shared input components

--- src/components/inputs/Add.vue
+++ src/components/inputs/Add.vue
@@ -1,36 +1,21 @@
 <template>
   <div class="mt-2 mb-2 pt-2 pb-2">
-    <p>Add new list</p>
+    <p>{{ title }}</p>
     <div class="flex justify-center">
       <input
         type="text"
-        v-model="listName"
-        @keyup.enter="addList"
-        placeholder="List name"
+        v-model="name"
+        @keyup.enter="add"
+        :placeholder="inputPlaceholder"
         class="border border-transparent bg-gray-100 border-green-100 rounded-sm focus:outline-none focus:border-green-500 focus:bg-white"
       />
       <button
         class="flex px-2 py-1 text-sm bg-green-400 rounded-sm text-white focus:outline-none disabled:opacity-50 disabled:cursor-default"
         :class="{ 'hover:bg-green-500': listNameExist }"
         :disabled="!listNameExist"
-        @click="addList"
+        @click="add"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          width="20"
-          height="20"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-          />
-        </svg>
-        <span class="align-middle">Add list</span>
+        {{ buttonLabel }}
       </button>
     </div>
   </div>
@@ -40,13 +25,25 @@
 export default {
   name: "AddList",
   props: {
+    buttonLabel: {
+      required: true,
+      type: String,
+    },
+    inputPlaceholder: {
+      required: true,
+      type: String,
+    },
     modelValue: {
+      required: true,
+      type: String,
+    },
+    title: {
       required: true,
       type: String,
     },
   },
   computed: {
-    listName: {
+    name: {
       get() {
         return this.modelValue;
       },
@@ -55,13 +52,13 @@ export default {
       },
     },
     listNameExist() {
-      return !!this.listName;
+      return !!this.name;
     },
   },
   methods: {
-    addList() {
+    add() {
       if (this.listNameExist) {
-        this.$emit("add-list");
+        this.$emit("add");
       }
     },
   },
