b98604bd9c4d5bfe8d6d0954a6fa0ed21f82b4d3
Andres Foronda <andresforonda.af@gmail.com>
2021-01-12 08:07:52 -0300

    Update AddList component in list admin

--- src/views/simple-todo-list/components/list-admin/Layout.vue
+++ src/views/simple-todo-list/components/list-admin/Layout.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="p-4 divide-y">
-    <h2 class="text-2xl">List admin</h2>
-    <add-list v-model="listName" @add-list="addList" />
+  <div class="p-4 divide-y border rounded-md mb-2">
+    <h2 class="text-2xl pt-2 pb-2">List admin</h2>
+    <add-list
+      @add="addList"
+      buttonLabel="Add list"
+      inputPlaceholder="List name"
+      title="Add new list"
+      v-model="listName"
+    />
     <select-list v-model="selectedListId" :lists="lists" />
   </div>
 </template>
 
 <script>
-import AddList from "./AddList.vue";
+import AddList from "@/components/inputs/Add.vue";
 import SelectList from "./SelectList.vue";
 export default {
   name: "ListAdmin",
