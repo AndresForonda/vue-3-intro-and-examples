2d1a914d8a2f55cf028568445b154eb42601943b
Andres Foronda <andresforonda.af@gmail.com>
2021-01-14 15:38:59 -0300

    Add colors for infiniteScroll to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#455D8C",
+        secondary: "#0F1926",
+        deepBlue: "#0F1926",
+        opaqueBlue: "#344F73",
+        ocre: "#BF8C2C",
+      },
+    },
   },
   variants: {
     extend: {
